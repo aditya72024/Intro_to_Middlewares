cd2a122cefb381e00e3cb6ef90afedbac27c012d
Aditya Ojha <aditya72024@gmail.com>
2023-09-05 19:54:33 +0530

    express-with-bodyParser

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const http = require('http');
 const express = require('express'); 
+const bodyParser = require('body-parser');
 const app = express();
-app.use((req,res,next) => {
-    console.log("I'm in a middleware");
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/',(req,res,next) => {
+    console.log("This always run!");
     next();
 })
 
 
-app.use((req,res,next) => {
-    console.log("I'm in aother middleware");
-    res.send("{ key1: value }")
+app.use('/add-product',(req,res,next) => {
+    console.log("Add Product");
+    res.send("<form action='/product' method='POST'><input type='text' name='title' placeholder='Product'><input type='text' name='Size in inch' placeholder='size'><button type='submit'>Submit</button></form>");
+})
+
+app.post('/product',(req,res,next) => {
+    console.log(req.body);
+    res.redirect("/");
+})
+
+app.use('/',(req,res,next) => {
+    console.log("Home");
+    res.send("<h1>Home</h1>")
 })
 
 app.listen(1500);
